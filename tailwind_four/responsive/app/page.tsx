a4fe8ab48f2d26f92c98f302ef41be92f59781c0
Mainuddin Akash <armainuddinakash1@gmail.com>
2025-11-27 10:23:08 +0600

    Tailwind 04: Responsive design

---
+++ tailwind_four/responsive/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-950 text-slate-50">
+      <h1 className="text-lg sm:text-4xl">Learn about responsive design</h1>
+    </div>
+  );
+}
