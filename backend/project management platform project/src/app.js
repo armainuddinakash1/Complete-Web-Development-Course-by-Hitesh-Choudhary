5bab7e1a9f180dab0de977b6cee0ffc613420713
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-01-16 12:47:54 +0600

    Backend: Project Management Platform Project: configuring express and CORS

--- backend/project management platform project/src/app.js
+++ backend/project management platform project/src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("welcome to basecamp")
-})
+// Basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("welcome to basecamp");
+});
 export default app;
