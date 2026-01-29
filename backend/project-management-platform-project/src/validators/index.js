6e1e916cb2ce33e9919881b12ac0a7d5c969b062
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-01-29 10:17:57 +0600

    feat: add custom validator for registering user
    Backend: Project Management Platform Project

---
+++ backend/project-management-platform-project/src/validators/index.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .toLowerCase()
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
