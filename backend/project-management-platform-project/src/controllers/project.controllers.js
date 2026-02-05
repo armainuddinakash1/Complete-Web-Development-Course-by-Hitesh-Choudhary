403599d80b23472507dd7b34a57f5219aa3900fc
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-02-05 11:06:55 +0600

    feat: add create, update, and delete project controllers
    Backend: Project Management Platform Project

--- backend/project-management-platform-project/src/controllers/project.controllers.js
+++ backend/project-management-platform-project/src/controllers/project.controllers.js
@@ -4,16 +4,62 @@ import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import mongoose from "mongoose";
+import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async (req, res) => {});
 
 const getProjectById = asyncHandler(async (req, res) => {});
 
-const createProject = asyncHandler(async (req, res) => {});
+const createProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
 
-const updateProject = asyncHandler(async (req, res) => {});
+  const project = await Project.create({
+    name,
+    description,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
 
-const deleteProject = asyncHandler(async (req, res) => {});
+  await ProjectMember.create({
+    user: new mongoose.Types.ObjectId(req.user._id),
+    project: new mongoose.Types.ObjectId(project._id),
+    role: UserRolesEnum.ADMIN,
+  });
+
+  res.status(201).json(201, project, "Project created successfully");
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const { projectId } = req.params;
+
+  const project = await Project.findByIdAndUpdate(
+    projectId,
+    { name, description },
+    { new: true },
+  );
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project updated successfully"));
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+  const project = await Project.findByIdAndDelete(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project deleted successfully"));
+});
 
 const addMembersToProject = asyncHandler(async (req, res) => {});
 
