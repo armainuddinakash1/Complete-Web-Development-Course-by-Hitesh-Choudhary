1c0d5f5bb7dd0b56f2071dbc6a621de87a6f3e63
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-04-04 12:05:48 +0600

    how to create your own react library

--- frontend/react/01-basic-vite/src/App.jsx
+++ frontend/react/01-basic-vite/src/App.jsx
@@ -1,8 +1,10 @@
 import Lorem from './Lorem'
 function App() {
+  const username = "mainuddinakash"
   return (
     <>
       <h1>Hello World</h1>
+      {username}
       <p>This is a paragraph</p>
       <Lorem />
     </>
--- frontend/react/01-basic-vite/src/main.jsx
+++ frontend/react/01-basic-vite/src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Visit Google",
+};
+
+// const root = document.getElementById('root');
+// customRender(reactElement, root);
+
+function MyApp() {
+  return (
+    <div>
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank" >Visit Google</a>
 )
+
+const realReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'click to visit google'
+)
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+  <App />
+  </StrictMode>,
+);
