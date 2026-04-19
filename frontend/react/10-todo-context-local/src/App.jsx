c65a90960009e54007c70a7ec9af6bbc573b2f5d
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-04-19 15:40:37 +0600

    small tailwind class correction

--- frontend/react/10-todo-context-local/src/App.jsx
+++ frontend/react/10-todo-context-local/src/App.jsx
@@ -4,7 +4,7 @@ import { TodoForm, TodoItems } from "./components";
 
 function AppContent() {
     return (
-        <div className="min-h-screen py-8 bg-gradient-to-br from-[#2d3436] to-[#636e72]">
+        <div className="min-h-screen py-8 bg-linear-to-br from-[#2d3436] to-[#636e72]">
             <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 bg-[#2d3436]">
                 <h1 className="text-2xl font-bold text-center text-white mb-8 mt-3">
                     Manage Your Todos
