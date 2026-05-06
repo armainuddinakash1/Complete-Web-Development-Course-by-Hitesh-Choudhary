08f2f55dc17a06fac62d2e4e88af6136b5630048
Mainuddin Akash <armainuddinakash1@gmail.com>
2026-05-06 19:10:18 +0600

    complete signup and login api route nextjs auth project

--- nextjs/auth-nextjs/src/app/login/page.tsx
+++ nextjs/auth-nextjs/src/app/login/page.tsx
@@ -3,19 +3,46 @@ import axios from "axios";
 import { on } from "events";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function Login() {
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: "",
     });
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const onLogin = async () => {};
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            toast("User logged in successfully");
+            router.push("/");
+        } catch (err: any) {
+            toast(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+    const notify = () => toast("Here is your toast.");
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-3xl mb-4">Login</h1>
+            <h1 className="text-3xl mb-4">
+                {loading ? "Processing..." : "Login"}
+            </h1>
             <label htmlFor="email">Email:</label>
             <input
                 type="email"
@@ -36,12 +63,13 @@ function Login() {
             />
             <button
                 onClick={onLogin}
-                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white disabled:bg-gray-400"
+                disabled={buttonDisabled}
             >
                 Login
             </button>
             <Link href="/signup" className="underline">
-                Signup 
+                Signup
             </Link>
         </div>
     );
--- nextjs/auth-nextjs/src/app/signup/page.tsx
+++ nextjs/auth-nextjs/src/app/signup/page.tsx
@@ -1,22 +1,50 @@
 "use client";
 import axios from "axios";
-import { on } from "events";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function Signup() {
+    const router = useRouter();
     const [user, setUser] = useState({
         username: "",
         email: "",
         password: "",
     });
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const onSignup = async () => {};
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            toast("User created successfully");
+            router.push("/");
+        } catch (err: any) {
+            toast(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (
+            user.email.length > 0 &&
+            user.username.length > 0 &&
+            user.password.length > 0
+        ) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-3xl mb-4">Signup</h1>
+            <h1 className="text-3xl mb-4">
+                {loading ? "Processing..." : "Signup"}
+            </h1>
             <label htmlFor="username">Username:</label>
             <input
                 type="text"
@@ -46,11 +74,14 @@ function Signup() {
             />
             <button
                 onClick={onSignup}
-                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white disabled:bg-gray-400"
+                disabled={buttonDisabled}
             >
                 Signup
             </button>
-            <Link href="/login" className="underline">Login page</Link>
+            <Link href="/login" className="underline">
+                Login page
+            </Link>
         </div>
     );
 }
